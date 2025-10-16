144a5835305f8ea4bbe807eb29d6dbe2804958da
whackamole7 <whackamole7@proton.me>
2025-10-16 16:55:43 +0300

    Updates

--- assets/chevron-right-DeBaFAFD.js
+++
@@ -1,8 +0,0 @@
-import{m as o}from"./core-qGSWr1kK.js";import"./index-FU8PrkYZ.js";import"./index.es-C8Gojhlb.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M4.96 14.54a1 1 0 0 1 0-1.41L10.09 8 4.96 2.87a1 1 0 0 1 1.41-1.41l5.84 5.83a1 1 0 0 1 0 1.42l-5.84 5.83a1 1 0 0 1-1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{i as chevronRightSvg};
